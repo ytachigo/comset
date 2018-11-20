8b6bb3fe65f2ccce12c3a8b124ff62e572b899ce
ytachigo <tachi.yuhei@gmail.com>
2018-11-21 05:39:23 +0900

    fixed

--- remdtemp.py
+++ remdtemp.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 def get_remdtemp():
-    n = input('Replica number: ')
-    mintmp = input('Minimum temperature (K): ')
-    maxtmp = input('Maximum temperature (K): ')
+    n = int(input('Replica number: '))
+    mintmp = int(input('Minimum temperature (K): '))
+    maxtmp = int(input('Maximum temperature (K): '))
     const = np.log(maxtmp / mintmp) / (n - 1)
     filename = 'temperatures.dat'
     f = open(filename, 'a')
 
     for i in range(0, n):
-        temp = mintmp * np.exp(const * n)
-        f.write(temp)
+        temp = mintmp * np.exp(const * i)
+        f.write(str(temp))
+        f.write('\n')
+    f.close()
 
 ##### Main Routine #####
-convert_1lto3l()
+get_remdtemp()
