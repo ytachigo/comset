6d7ae02f9f7d10ef10fbc3aa639dcbc14cbaafe6
ytachigo <tachi.yuhei@gmail.com>
2019-05-03 23:19:56 +0900

    add a new file

--- move_water.py
+++ move_water.py
@@ -1,15 +1,30 @@
 import numpy as np
 
-def get_getgroupatoms():
-    firstatom = int(input('First number of atoms: '))
-    lastatom = int(input('Last number of atoms: '))
-    filename = 'groupatoms.dat'
-    f = open(filename, 'a')
+def move_water(): # Move water molecules
+    maxcoord = 40
+    mincoord = -40
+    addcoord = 100
 
-    for i in range(firstatom, lastatom + 1):
-        atomnum = '%d, ' % i
-        f.write(atomnum)
-    f.close()
+    filename = input('Filename(pdb): ')
+    open_infile = open(filename, 'r')
+
+    for kline in open_infile.readlines():
+        output = 'move_water.pdb'
+        open_outfile = open(output, 'a')
+
+        if kline[0:4] == 'ATOM' or kline[0:6] == 'HETATOM':
+            if str(kline[16:20].strip()) == 'TIP3' and \
+            maxcoord >= float(kline[47:54]) and mincoord <= float(kline[47:54]):
+                newcoord = str(round((float(kline[47:54]) + addcoord), 3))
+                for i in range(0, 7 - len(newcoord) + 1):
+                    newcoord = ' ' + newcoord
+                nline = kline[:46] +  newcoord + kline[54:]
+                open_outfile.write(nline)
+
+            else: open_outfile.write(kline)
+        else: open_outfile.write(kline)
+    open_outfile.close()
+    open_infile.close()
 
 ##### Main Routine #####
-get_getgroupatoms()
+move_water()
